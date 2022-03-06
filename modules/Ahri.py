2712ec7f757910c040027d973b16276ed058fbdb
ImpulZzZ <lukas.drothler@web.de>
2022-03-06 14:15:01 +0100

    added base stats and mpen

--- modules/Ahri.py
+++ modules/Ahri.py
@@ -7,11 +7,68 @@ class Ahri():
         self.name       = "ahri"
         self.name_upper = "Ahri"
 
+        self.ap    = 0
+        self.m_pen = 0
+        self.tiers = {
+            "q": 1,
+            "w": 1,
+            "e": 1,
+            "r": 1,
+        }
+
         ## Fetch data from API
         data     = Data()
         url      = f"{data.base_url}/{data.patch}/game/data/characters/{self.name}/{self.name}.bin.json"
         response = requests.get(url)
         self.response_data = json.loads(response.text)
+
+    def get_ap(self):
+        return self.ap
+
+    def get_m_pen(self):
+        return self.ap
+
+    def get_tiers(self):
+        return self.tiers
+
+    def set_ap(self, ap):
+        if ap >= 0: self.ap = ap
+        else: print(f"Cannot set ap to {ap}")
+        return None
+
+    def set_m_pen(self, m_pen):
+        if m_pen >= 0: self.m_pen = m_pen
+        else: print(f"Cannot set m_pen to {m_pen}")
+        return None
+
+    def add_ap(self, ap):
+        if self.ap + ap >= 0: self.ap += ap
+        else: print(f"Cannot add {ap} ap")
+        return None
+
+    def add_m_pen(self, m_pen):
+        if self.m_pen + m_pen >= 0: self.m_pen += m_pen
+        else: print(f"Cannot add {m_pen} m_pen")
+        return None
+
+    def set_tiers(self, tiers):
+        try:
+            if tiers["q"] not in [1,2,3,4,5] or tiers["w"] not in [1,2,3,4,5] or tiers["e"] not in [1,2,3,4,5] or tiers["r"] not in [1,2,3]:     
+                print(f"Invalid tiers: {tiers}")
+                return None
+            else: self.tiers = tiers
+        except KeyError:
+            print(f"Invalid tiers: {tiers}")
+        return None
+
+    def add_tier(self, spell, amount):
+        try:
+            if (spell in ["q","w","e"] and self.tiers[spell] + amount not in [1,2,3,4,5]) or (spell == "r" and self.tiers[spell] + amount not in [1,2,3]):
+                print(f"Invalid spell: {spell}")
+                return None
+        except KeyError:
+            print(f"Invalid spell: {spell}")
+        return None
 
 
     def invalid_tier_or_ap(self, tier, ap, spell_name, missiles=None, isUlt=False):
@@ -26,28 +83,29 @@ class Ahri():
                 print(f"Invalid missiles '{missiles}' for {spell_name} given")
                 return True
 
-    
-    def q(self, tier, ap):
+    def q(self):
         spell_name = "AhriOrbofDeception"
+        tier = self.tiers['q']
 
-        if self.invalid_tier_or_ap(tier, ap, spell_name): return None
+        if self.invalid_tier_or_ap(self.tiers['q'], self.ap, spell_name): return None
 
         for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mDataValues"]:
             try:
                 if value["mName"] == "BaseDamage": damages = value["mValues"][1:6]
             except KeyError: continue
 
-        for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mSpellCalculations"]["TotalDamage"]["mFormulaParts"]:
+        for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mSpellCalculations"]["TotalDamage"]["mFormulparts"]:
             try: 
                 if value["mCoefficient"] is not None: scaling = value["mCoefficient"]
             except KeyError: continue
 
-        return damages[tier - 1] + ap * scaling
+        return damages[tier - 1] + self.ap * scaling
     
-    def w(self, tier, ap, missiles):
+    def w(self, missiles):
         spell_name = "AhriFoxFire"
+        tier = self.tiers['w']
 
-        if self.invalid_tier_or_ap(tier, ap, spell_name, missiles): return None
+        if self.invalid_tier_or_ap(tier, self.ap, spell_name, missiles): return None
 
         for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mDataValues"]:
             try:
@@ -63,16 +121,17 @@ class Ahri():
         damage = 0
         for missile in range(1, missiles+1):
             ## first missile does full damage
-            if missile == 1: damage += (damages[tier-1] + scaling * ap)
+            if missile == 1: damage += (damages[tier-1] + scaling * self.ap)
             ## second and third missile damage are reduced
-            else:            damage += (damage_reduction[tier-1]*(damages[tier-1] + scaling * ap))
+            else:            damage += (damage_reduction[tier-1]*(damages[tier-1] + scaling * self.ap))
 
         return damage
 
-    def e(self, tier, ap):
+    def e(self):
         spell_name = "AhriSeduce"
+        tier = self.tiers['w']
 
-        if self.invalid_tier_or_ap(tier, ap, spell_name): return None
+        if self.invalid_tier_or_ap(tier, self.ap, spell_name): return None
 
         for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mDataValues"]:
             try:
@@ -85,12 +144,13 @@ class Ahri():
                 if value["mCoefficient"] is not None: scaling = value["mCoefficient"]
             except KeyError: continue
 
-        return damages[tier - 1] + ap * scaling
+        return damages[tier - 1] + self.ap * scaling
 
-    def r(self, tier, ap, missiles):
+    def r(self, missiles):
         spell_name = "AhriTumbleAbility/AhriTumble"
+        tier = self.tiers['w']
 
-        if self.invalid_tier_or_ap(tier, ap, spell_name, missiles, isUlt=True): return None
+        if self.invalid_tier_or_ap(tier, self.ap, spell_name, missiles, isUlt=True): return None
 
         for value in self.response_data[f"Characters/{self.name_upper}/Spells/{spell_name}"]["mSpell"]["mDataValues"]:
             try:
@@ -98,12 +158,4 @@ class Ahri():
                 if value["mName"] == "RAPCoefficient": scalings = value["mValues"][1:4]
             except KeyError: continue
 
-        return (damages[tier-1] + scalings[tier-1] * ap) * missiles
-
-
-        
-
-
-
-
-
+        return (damages[tier-1] + scalings[tier-1] * self.ap) * missiles
